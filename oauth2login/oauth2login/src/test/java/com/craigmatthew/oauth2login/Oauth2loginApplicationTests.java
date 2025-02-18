65eb0dd08e9c91707ea36e22c14f356b5dd378c8
LAB14_USER ON WS34 <L14Y18W34@citulab.net>
2025-02-18 18:48:43 +0800

    initial springboot oauth2

---
+++ oauth2login/oauth2login/src/test/java/com/craigmatthew/oauth2login/Oauth2loginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.craigmatthew.oauth2login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2loginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
